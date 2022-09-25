13a32f704605089fdfd64743d29595eddca69b9f
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-25 21:37:59 +0300

    feat: коммит на сервак

---
+++ frontend/build/precache-manifest.80d61b736fa448cc767f41316fe34c2a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26788dffe431ece601287ed0a4f4dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "992f37590af8ac0d3ad2",
+    "url": "/static/css/main.a8197efb.chunk.css"
+  },
+  {
+    "revision": "12b97900a98deae49bbb",
+    "url": "/static/js/2.9acfea53.chunk.js"
+  },
+  {
+    "revision": "992f37590af8ac0d3ad2",
+    "url": "/static/js/main.4bdfeb92.chunk.js"
+  },
+  {
+    "revision": "0d5e2f14c5d592885ee5",
+    "url": "/static/js/runtime-main.458ad500.js"
+  },
+  {
+    "revision": "4c6a21695c10c9c77e7676ffa4a389c1",
+    "url": "/static/media/Close-Icon.4c6a2169.svg"
+  },
+  {
+    "revision": "7e6320671143a712e48cac8f42b6379a",
+    "url": "/static/media/Inter-Black.7e632067.woff2"
+  },
+  {
+    "revision": "c5b5ffbc9dbd7624c91f7a66e8260edf",
+    "url": "/static/media/Inter-Black.c5b5ffbc.woff"
+  },
+  {
+    "revision": "20086520d0b0924030c8b4c192986677",
+    "url": "/static/media/Inter-Medium.20086520.woff"
+  },
+  {
+    "revision": "69d904ddd2fd64c31b065ef06f0c5c13",
+    "url": "/static/media/Inter-Medium.69d904dd.woff2"
+  },
+  {
+    "revision": "64d9bbdb68244c2812e0b90f07a499e9",
+    "url": "/static/media/Inter-Regular.64d9bbdb.woff2"
+  },
+  {
+    "revision": "d9c95941d299e8c9459fece87664ec0d",
+    "url": "/static/media/Inter-Regular.d9c95941.woff"
+  },
+  {
+    "revision": "223359275f4bedbe62175da6ebac13d5",
+    "url": "/static/media/Logo.22335927.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/static/media/add-button.bdfb8ebb.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/static/media/button-edit.58afbe73.svg"
+  },
+  {
+    "revision": "933106eaf93ffbb3807048cfeeecbcef",
+    "url": "/static/media/like-black.933106ea.svg"
+  },
+  {
+    "revision": "72781e3cdbf75fc1e67979369ef0aebe",
+    "url": "/static/media/like.72781e3c.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/static/media/pencilAvatar.34c46850.svg"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "/static/media/recycle.9c3b03ae.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/static/media/union.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/static/media/ununion.4072126c.svg"
+  }
+]);
