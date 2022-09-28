0b291fa586aaef8bd089710a5061e4374a91c9a8
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-28 11:02:22 +0300

    feat: коммит на сервак

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors, celebrate, Joi } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { PORT = 4000 } = process.env;
 const app = express();
@@ -22,46 +22,43 @@ app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// const allowedCors = [
-//   'https://mestofullgha.nomorepartiesxyz.ru',
-//   'http://mestofullgha.nomorepartiesxyz.ru',
-//   'localhost:3000',
-// ];
-
-// app.use((req, res, next) => {
-//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-//   Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   // Если это предварительный запрос, добавляем нужные заголовки
-//   if (method === 'OPTIONS') {
-//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     // разрешаем кросс-доменные запросы с этими заголовками
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     // завершаем обработку запроса и возвращаем результат клиенту
-//     return res.end();
-//   }
-//   const { origin } = req.headers;
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   return next();
-// });
-
 app.use(requestLogger);
 
-app.use(cors(
-  {
-    // eslint-disable-next-line quotes
-    origin: "https://mestofullgha.nomorepartiesxyz.ru/",
-    // eslint-disable-next-line quotes
-    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-    // eslint-disable-next-line quotes
-    allowedHeaders: "access-control-request-headers, Content-Type, Authorization",
-    credentials: true,
-  },
-));
+const allowedCors = [
+  'https://mestofullgha.nomorepartiesxyz.ru',
+  'http://mestofullgha.nomorepartiesxyz.ru',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const { origin } = req.headers;
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    return res.end();
+  }
+  return next();
+});
+
+// app.use(cors(
+//   {
+//     // eslint-disable-next-line quotes
+//     origin: "https://mestofullgha.nomorepartiesxyz.ru/",
+//     // eslint-disable-next-line quotes
+//     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+//     // eslint-disable-next-line quotes
+//     allowedHeaders: "access-control-request-headers, Content-Type, Authorization",
+//     credentials: true,
+//   },
+// ));
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
