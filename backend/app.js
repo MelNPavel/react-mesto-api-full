5edacffa1a0e1e841884210271a8f2600ef0cd33
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-28 15:18:39 +0300

    feat: коммит на сервак

--- backend/app.js
+++ backend/app.js
@@ -38,10 +38,12 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-    }
+    return res.end();
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
   return next();
