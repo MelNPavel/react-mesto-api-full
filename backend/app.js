9a46e680f0a75a5252883ff0a0c415676f3fc2f2
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-28 15:12:50 +0300

    feat: коммит на сервак

--- backend/app.js
+++ backend/app.js
@@ -36,14 +36,12 @@ app.use((req, res, next) => {
   // eslint-disable-next-line no-sequences
   const requestHeaders = req.headers['access-control-request-headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'];
   const { origin } = req.headers;
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+    }
     return res.end();
   }
   return next();
