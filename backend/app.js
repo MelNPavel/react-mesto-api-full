17f4997fb43de22448771733a08653add7aba27e
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-28 15:16:34 +0300

    feat: коммит на сервак

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   // eslint-disable-next-line no-sequences
-  const requestHeaders = req.headers['access-control-request-headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'];
+  const requestHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
