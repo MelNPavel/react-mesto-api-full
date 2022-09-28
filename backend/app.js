82c3073aaf54910fb0b8880a066a23a960250243
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-28 08:42:16 +0300

    feat: коммит на сервак

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ app.use(
   cors({
     origin: 'https://localhost:3000',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    allowedHeaders: ['access-control-request-headers'],
     credentials: true,
   }),
 );
